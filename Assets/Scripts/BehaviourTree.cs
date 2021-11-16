c3e680ea09773db43b86481f2d48bd474e8b353f
AdrianRamosBoira <78815548+AdrianRamosBoira@users.noreply.github.com>
2021-11-16 15:16:36 +0100

    TaskClass

---
+++ Assets/Scripts/BehaviourTree.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct nodeTask
+{
+    public TaskCollection task;
+    public TaskCollection father;
+
+    public nodeTask(TaskCollection newTask, TaskCollection newFather)
+    {
+        task = newTask;
+        father = newFather;
+    }
+}
+
+public class BehaviourTree
+{
+    public TaskCollection rootTask;
+
+    private List<nodeTask> treeConstructor;
+
+    public BehaviourTree()
+    {
+        rootTask = new Selector();
+        treeConstructor = new List<nodeTask>();
+        nodeTask currentTask;
+        int index = 0;
+
+        //Primer hijo del nodo raíz
+        currentTask = new nodeTask(new Sequence(), rootTask);
+        treeConstructor[index] = currentTask;
+        index++;
+    }
+
+    public void buildBehaviourTree()
+    {
+        for(int i = 0; i < treeConstructor.Count; i++)
+        {
+            treeConstructor[i].father.children.Add(treeConstructor[i].task);
+        }
+    }
+}
---
+++ Assets/Scripts/TaskCollection.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TaskCollection
+{
+    public List<TaskCollection> children
+    {
+        get
+        {
+            return children;
+        }
+
+        set
+        {
+            children = value;
+        }
+    }
+
+    public void setChildren(List<TaskCollection> newChildren) 
+    {
+        foreach (var child in newChildren)
+            children.Add(child);
+    }
+
+    public abstract bool Run();
+}
+
+public class Selector : TaskCollection
+{
+    public override bool Run()
+    {
+        for (int i = 0; i < children.Count; i++)
+        {
+            if (children[i].Run())
+                return true;
+        }
+
+        return false;
+    }
+}
+
+public class Sequence : TaskCollection
+{
+    public override bool Run()
+    {
+        for (int i = 0; i < children.Count; i++)
+        {
+            if (!children[i].Run())
+                return false;
+        }
+
+        return true;
+    }
+}
+
+public abstract class Decorator : TaskCollection
+{
+    public abstract TaskCollection child { get; set; }
+}
+
+public class UntilFail : Decorator
+{
+    private TaskCollection _child;
+    public override TaskCollection child
+    {
+        get
+        {
+            return _child;
+        }
+
+        set
+        {
+            _child = value;
+        }
+    }
+
+    public override bool Run()
+    {
+        while (true)
+        {
+            if (!_child.Run())
+                break;
+        }
+
+        return true;
+    }
+}
+
+
